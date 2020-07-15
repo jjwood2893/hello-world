ba603c8d2d47b7f53648b000cafe1e50a9c07f13
Jacob W <jjwood2893@gmail.com>
2020-07-14 20:39:59 -0400

    standardizing commits

---
+++ Pathfinder 2e Character Sheet/Alchemist.java
@@ -0,0 +1,27 @@
+
+public class Alchemist extends ClassOverview {
+	private String perception = "trained";
+	private String fortSavingThrow = "expert";
+	private String reflexSavingThrow = "expert";
+	private String willSavingThrow = "trained";
+	private String classSkill1 = "crafting";
+	private String classSkill2 = null;
+	private int extraSkills = 3;
+	private String proficiencySimple = "trained";
+	private String proficiencyMartial = "untrained";
+	private String proficiencySpecial = "trained";
+	private String proficiencyUnarmed = "trained";
+	private String specialTrait = "bomb";
+	private String proficiencyClass = "trained";
+	private String proficiencyLight = "trained";
+	private String proficiencyMedium = "untrained";
+	private String proficiencyHeavy = "untrained";
+	private String proficiencyUnarmored;
+	private String spellTraining;
+	private String spellDCTraining;
+	
+	public Alchemist() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Pathfinder 2e Character Sheet/ClassOverview.java
@@ -0,0 +1,84 @@
+
+abstract class ClassOverview {
+	
+	private String perception = "trained";
+	private String fortSavingThrow = "trained";
+	private String reflexSavingThrow = "trained";
+	private String willSavingThrow = "trained";
+	private String classSkill1;
+	private String classSkill2;
+	private int extraSkills;
+	private String proficiencySimple = "trained";
+	private String proficiencyMartial;
+	private String proficiencySpecial;
+	private String proficiencyUnarmed = "trained";
+	private String proficiencyClass = "trained";
+	private String proficiencyLight;
+	private String proficiencyMedium;
+	private String proficiencyHeavy;
+	private String proficiencyUnarmored = "trained";
+	private String spellTraining;
+	private String spellDCTraining;
+	
+	protected ClassOverview() {
+		
+	}
+	
+	public String getPerception() {
+		return perception;
+	}
+	public String getFortSavingThrow() {
+		return fortSavingThrow;
+	}
+	public String getReflexSavingThrow() {
+		return reflexSavingThrow;
+	}
+	public String getWillSavingThrow() {
+		return willSavingThrow;
+	}
+	public String getClassSkill1() {
+		return classSkill1;
+	}
+	public String getClassSkill2() {
+		return classSkill2;
+	}
+	public int getExtraSkills() {
+		return extraSkills;
+	}
+	public String getProficiencySimple() {
+		return proficiencySimple;
+	}
+	public String getProficiencyMartial() {
+		return proficiencyMartial;
+	}
+	public String getProficiencyClass() {
+		return proficiencyClass;
+	}
+	public String getProficiencyLight() {
+		return proficiencyLight;
+	}
+	public String getProficiencyMedium() {
+		return proficiencyMedium;
+	}
+	public String getProficiencyHeavy() {
+		return proficiencyHeavy;
+	}
+	public String getProficiencyUnarmored() {
+		return proficiencyUnarmored;
+	}
+	public String getSpellTraining() {
+		return spellTraining;
+	}
+	public String getSpellDCTraining() {
+		return spellDCTraining;
+	}
+
+	public String getProficiencySpecial() {
+		return proficiencySpecial;
+	}
+
+	public String getProficiencyUnarmed() {
+		return proficiencyUnarmed;
+	}
+	
+}
